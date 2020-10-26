52ca8f10502932916ff1103e09dbea5f25d5f7b8
Tomasz Kraszewski <Snakerrek@gmail.com>
2020-10-26 19:05:29 +0100

    Basic player movement

---
+++ Spinning Shapes/Assets/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 300f;
+
+    void FixedUpdate()
+    {
+        if(Input.GetKey("a"))
+            transform.RotateAround(Vector3.zero, Vector3.forward, moveSpeed * Time.fixedDeltaTime);
+        if (Input.GetKey("d"))
+            transform.RotateAround(Vector3.zero, Vector3.forward, -moveSpeed * Time.fixedDeltaTime);
+    }
+}
